5c3ac9d685507d8b05acadaf7b074f22d79d59c9
avishena <avishena1993@gmail.com>
2018-11-24 22:11:13 +0700

    [SFX - Console]

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -17,29 +17,47 @@ public class GameController : MonoBehaviour {
     public GameObject questionDisplay;
     public GameObject roundEndDisplay;
     public Text highScoreDisplay;
+    public Text playerScoreDisplay;
 
     public Animator playerAnimator;
     public Animator enemyAnimator;
 
-    public Slider playerHealth;
+    public Text playerHealth;
+    public Text enemyTime;
     public Slider enemyHealth;
-    public Slider enemyTime;
+
+    public AudioClip attackSound;
+    public AudioClip dragonSound;
+    public AudioClip rightHookSound;
+    public AudioClip leftHookSound;
+    public AudioClip clickSound;
 
     private LevelScreenController dataController;
     private RoundData currentRoundData;
     private QuestionData[] questionPool;
 
     private bool isRoundActive;
+    private bool playerAttackStat = false;
+    private bool playerDieStat = false;
+    private bool enemyAttackStat = false;
     private float timeRemaining;
     private float playerHealthRemaining;
     private float enemyHealthRemaining;
     private int questionIndex;
     private int playerScore;
     private Sprite questionImage;
+    private AudioSource source;
     private List<GameObject> answerButtonGameObjects = new List<GameObject>();
 
-	// Use this for initialization
-	void Start ()
+    float counter = 0;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    void Start ()
     {
         questionText.enabled = false;
         questionTextImage.enabled = false;
@@ -49,17 +67,18 @@ public class GameController : MonoBehaviour {
         questionPool = currentRoundData.questions;
 
         timeRemaining = currentRoundData.timeLimitInSeconds;
-        enemyTime.maxValue = timeRemaining;
-        enemyTime.minValue = 0f;
+        // enemyTime.maxValue = timeRemaining;
+        // enemyTime.minValue = 0f;
         UpdateTimeRemainingDisplay();
-
         playerHealthRemaining = 30f;
-        playerHealth.maxValue = playerHealthRemaining;
-        playerHealth.minValue = 0f;
+        playerHealth.text = playerHealthRemaining.ToString();
+        // playerHealth.maxValue = playerHealthRemaining;
+        // playerHealth.minValue = 0f;
         UpdatePlayerHealthDisplay();
 
         enemyHealthRemaining = 30f;
         enemyHealth.maxValue = enemyHealthRemaining;
+
         enemyHealth.minValue = 0f;
         UpdateEnemyHealthDisplay();
 
@@ -95,7 +114,7 @@ public class GameController : MonoBehaviour {
         {
             GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();
             answerButtonGameObjects.Add(answerButtonGameObject);
-            answerButtonGameObject.transform.SetParent(answerButtonParent);
+            answerButtonGameObject.transform.SetParent(answerButtonParent, false);
 
             AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
             answerButton.Setup(questionData.answers[i]);
@@ -113,30 +132,38 @@ public class GameController : MonoBehaviour {
 
     public void AnswerButtonClicked(bool isCorrect)
     {
+        source.PlayOneShot(clickSound, 1f);
         if (isCorrect)
         {
+            playerAttackStat = true;
             playerAnimator.SetTrigger("attack");
-            enemyAnimator.SetTrigger("attacked");
+            source.PlayOneShot(attackSound, 1f);
 
             playerScore += currentRoundData.pointsAddedForCorrectAnswer;
-            scoreDisplayText.text = "Score: " + playerScore.ToString();
+            scoreDisplayText.text = playerScore.ToString();
 
-            enemyHealthRemaining -= currentRoundData.pointsAddedForCorrectAnswer;
-            UpdateEnemyHealthDisplay();
+            // enemyHealthRemaining -= currentRoundData.pointsAddedForCorrectAnswer;
+            // UpdateEnemyHealthDisplay();
 
             timeRemaining = currentRoundData.timeLimitInSeconds;
             UpdateTimeRemainingDisplay();
 
         } else
         {
+            enemyAttackStat = true;
             enemyAnimator.SetTrigger("attack");
-            playerAnimator.SetTrigger("attacked");
+            source.PlayOneShot(dragonSound, 1f);
 
             playerHealthRemaining -= currentRoundData.pointsAddedForCorrectAnswer;
             UpdatePlayerHealthDisplay();
 
             timeRemaining = currentRoundData.timeLimitInSeconds;
             UpdateTimeRemainingDisplay();
+
+            if (playerHealthRemaining <= 0)
+            {
+                PlayerDead();
+            }
         }
 
         if (questionPool.Length > questionIndex + 1)
@@ -152,8 +179,9 @@ public class GameController : MonoBehaviour {
     public void EndRound()
     {
         isRoundActive = false;
+        playerScoreDisplay.text = "" + playerScore;
         dataController.SubmitNewPlayerScore(playerScore, "highestScore" + currentRoundData.subject);
-        highScoreDisplay.text = "High Score: " + dataController.getHighestPlayerScore("highestScore" + currentRoundData.subject).ToString();
+        highScoreDisplay.text = "" + dataController.getHighestPlayerScore("highestScore" + currentRoundData.subject).ToString();
 
         questionDisplay.SetActive(false);
         roundEndDisplay.SetActive(true);
@@ -166,18 +194,26 @@ public class GameController : MonoBehaviour {
 
     private void UpdateTimeRemainingDisplay()
     {
-        //timeRemainingDisplayText.text = "Time: " + Mathf.Round(timeRemaining).ToString();
-        enemyTime.value = timeRemaining;
+        // timeRemainingDisplayText.text = "Time: " + Mathf.Round(timeRemaining).ToString();
+        // enemyTime.value = timeRemaining;
+        enemyTime.text = Mathf.Round(timeRemaining).ToString();
     }
 
     private void UpdatePlayerHealthDisplay()
     {
-        playerHealth.value = playerHealthRemaining;
+        // playerHealth.value = playerHealthRemaining;
+        playerHealth.text = playerHealthRemaining.ToString();
     }
 
     private void UpdateEnemyHealthDisplay()
     {
         enemyHealth.value = enemyHealthRemaining;
+    }
+
+    private void PlayerDead()
+    {
+        playerDieStat = true;
+        playerAnimator.SetTrigger("die");
     }
 
     // Update is called once per frame
@@ -190,8 +226,9 @@ public class GameController : MonoBehaviour {
 
             if (timeRemaining <= 0f)
             {
+                enemyAttackStat = true;
                 enemyAnimator.SetTrigger("attack");
-                playerAnimator.SetTrigger("attacked");
+                source.PlayOneShot(dragonSound, 1f);
 
                 playerHealthRemaining -= currentRoundData.pointsAddedForCorrectAnswer;
                 UpdatePlayerHealthDisplay();
@@ -199,13 +236,42 @@ public class GameController : MonoBehaviour {
                 timeRemaining = currentRoundData.timeLimitInSeconds;
                 UpdateTimeRemainingDisplay();
 
-                if (questionPool.Length > questionIndex + 1)
+                if (playerHealthRemaining > 0 && questionPool.Length > questionIndex + 1)
                 {
                     questionIndex++;
                     ShowQuestion();
                 }
                 else
                 {
+                    PlayerDead();
+                }
+            }
+
+            if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("attack")) {
+
+                if (playerAttackStat == true && playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f)
+                {
+                    enemyAnimator.SetTrigger("attacked");
+                    source.PlayOneShot(leftHookSound, 1f);
+                    playerAttackStat = false;
+                }
+            }
+
+            if (enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("attack"))
+            {
+                if (enemyAttackStat == true && enemyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f)
+                {
+                    playerAnimator.SetTrigger("attacked");
+                    source.PlayOneShot(rightHookSound, 1f);
+                    enemyAttackStat = false;
+                }
+            }
+
+            if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("die"))
+            {
+                if (playerDieStat == true && playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
+                {
+                    playerDieStat = false;
                     EndRound();
                 }
             }
